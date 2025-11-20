a75221bbda552964d85290e216306245407698d9
Dua <doaazhary1@gmail.com>
2025-11-20 17:50:57 +0300

    Add all project files and structure for team

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,12 +1,12 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "traveldream_db";
+$username   = "root";
+$password   = "";
+$dbname     = "travel_explorer_db";  // اسم قاعدة البيانات الجديدة
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("فشل الاتصال بقاعدة البيانات: " . $conn->connect_error);
+    die("❌ Connection failed: " . $conn->connect_error);
 }
 ?>
